1e0465c684d60416a530c92990112d49b6d3505c
calmwu <wubo0067@hotmail.com>
2019-03-25 22:05:42 +0800

    k8s cluster需要的image

--- pull-images.sh
+++ pull-images.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
-images=(kubernetes-apiserver:v1.13.4 kubernetes-controller-manager:v1.13.4 kubernetes-scheduler:v1.13.4 kubernetes-proxy:v1.13.4 pause:3.1 etcd:3.2.24)
+docker pull openstackmagnum/kubernetes-apiserver:v1.13.4
+docker tag openstackmagnum/kubernetes-apiserver:v1.13.4 k8s.gcr.io/kube-apiserver:v1.13.4
+docker rmi openstackmagnum/kubernetes-apiserver:v1.13.4
 
-for ima in ${images[@]}
-do
-   docker pull   openstackmagnum/$ima
-   docker tag    openstackmagnum/$ima   k8s.gcr.io/$ima
-   docker rmi  -f  openstackmagnum/$ima
-done
+docker pull openstackmagnum/kubernetes-controller-manager:v1.13.4
+docker tag openstackmagnum/kubernetes-controller-manager:v1.13.4 k8s.gcr.io/kube-controller-manager:v1.13.4
+docker rmi openstackmagnum/kubernetes-controller-manager:v1.13.4
+
+docker pull openstackmagnum/kubernetes-scheduler:v1.13.4
+docker tag openstackmagnum/kubernetes-scheduler:v1.13.4 k8s.gcr.io/kube-scheduler:v1.13.4
+docker rmi openstackmagnum/kubernetes-scheduler:v1.13.4
+
+docker pull openstackmagnum/kubernetes-proxy:v1.13.4
+docker tag openstackmagnum/kubernetes-proxy:v1.13.4 k8s.gcr.io/kube-proxy:1.2.6
+docker rmi openstackmagnum/kubernetes-proxy:v1.13.4
+
+docker pull openstackmagnum/pause:3.1
+docker tag openstackmagnum/pause:3.1 k8s.gcr.io/pause:3.1
+docker rmi openstackmagnum/pause:3.1
+
+docker pull ibmcom/etcd:3.2.24
+docker tag ibmcom/etcd:3.2.24 k8s.gcr.io/etcd:3.2.24
+docker rmi ibmcom/etcd:3.2.24
 
 docker pull coredns/coredns:1.2.6
 docker tag coredns/coredns:1.2.6 k8s.gcr.io/coredns:1.2.6
@@ -15,34 +30,3 @@ docker rmi coredns/coredns:1.2.6
 docker pull jmgao1983/flannel:v0.11.0-amd64
 docker tag jmgao1983/flannel:v0.11.0-amd64 quay.io/coreos/flannel:v0.11.0-amd64
 docker rmi jmgao1983/flannel:v0.11.0-amd64
-
-////////////////////////////////////////////////////////////////////////////////////
-
-kubeadm join 192.168.6.131:6443 --token vawgip.ysfbx5kfogpb3u0o --discovery-token-ca-cert-hash sha256:43efc95b8d3edc43b3f3fdf6233395f593af48b8ac3a32c19bb7742bdf4b9160 --ignore-preflight-errors=Swap
-
-vim /etc/docker/daemon.conf 加入{"registry-mirrors": ["http://f1361db2.m.daocloud.io"]}
-
-curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io
-
-我把kube-flannel.yml依赖的镜像版本从v0.11改为v0.10，我从这里下载的，docker pull cnych/flannel:v0.10.0-amd64
-
-docker pull jmgao1983/flannel:v0.11.0-amd64
-
-
-[root@k8snode2 calmwu]# docker pull mirrorgooglecontainers/pause:3.1
-3.1: Pulling from mirrorgooglecontainers/pause
-67ddbfb20a22: Pull complete 
-Digest: sha256:59eec8837a4d942cc19a52b8c09ea75121acc38114a2c68b98983ce9356b8610
-Status: Downloaded newer image for mirrorgooglecontainers/pause:3.1
-[root@k8snode2 calmwu]# docker tag mirrorgooglecontainers/pause:3.1 k8s.gcr.io/pause:3.1
-[root@k8snode2 calmwu]# docker rmi mirrorgooglecontainers/pause:3.1
-
-docker pull mirrorgooglecontainers/kube-proxy:v1.13.2
-docker tag mirrorgooglecontainers/kube-proxy:v1.13.2 k8s.gcr.io/kube-proxy:v1.13.2
-docker rmi mirrorgooglecontainers/kube-proxy:v1.13.2
-
-如果子节点起不来就重启下该服务
-systemctl restart kubelet
-
-让master可以作为工作节点
-kubectl taint nodes --all node-role.kubernetes.io/master-
