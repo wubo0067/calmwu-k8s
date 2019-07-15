3bb308921bb3298d9bc96f73a27ed3f0da3fd305
littlebull <calm.wu@qq.com>
2019-07-15 13:59:57 +0800

    提交

---
+++ gas/cmd/gateway/gateway.go
@@ -0,0 +1,14 @@
+/*
+ * @Author: calm.wu
+ * @Date: 2019-07-09 15:01:08
+ * @Last Modified by: calm.wu
+ * @Last Modified time: 2019-07-09 15:35:20
+ */
+
+package main
+
+import "gas/pkg/gateway"
+
+func main() {
+	gateway.Main()
+}
