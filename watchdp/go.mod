da20dfeeda355c771e170b6874eafead9d8243c5
littlebull <calm.wu@qq.com>
2020-03-22 13:29:40 +0800

    1：watchdp

---
+++ watchdp/go.mod
@@ -0,0 +1,31 @@
+module calmwu.org/watchdp
+
+go 1.13
+
+require (
+	github.com/DeanThompson/ginpprof v0.0.0-20190408063150-3be636683586 // indirect
+	github.com/cheekybits/genny v1.0.0 // indirect
+	github.com/fatih/color v1.9.0 // indirect
+	github.com/gin-gonic/gin v1.5.0 // indirect
+	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/gorilla/websocket v1.4.1 // indirect
+	github.com/imdario/mergo v0.3.8 // indirect
+	github.com/micro/cli v0.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/monnand/dhkx v0.0.0-20180522003156-9e5b033f1ac4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.4.2 // indirect
+	github.com/snwfdhmp/errlog v0.0.0-20191219134421-4c9e67f11ebc // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/urfave/cli v1.22.2
+	github.com/wubo0067/calmwu-go v0.0.0-20200219020446-0236a7792879
+	go.uber.org/zap v1.13.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	k8s.io/api v0.17.3
+	k8s.io/apimachinery v0.17.3
+	k8s.io/client-go v0.17.3
+	k8s.io/utils v0.0.0-20200124190032-861946025e34 // indirect
+	sigs.k8s.io/yaml v1.2.0 // indirect
+)
---
+++ watchdp/watchdp.go
@@ -0,0 +1,92 @@
+/*
+ * @Author: calm.wu
+ * @Date: 2020-02-20 15:34:17
+ * @Last Modified by: calm.wu
+ * @Last Modified time: 2020-02-20 15:44:14
+ */
+
+// Package main for watch Deployment
+package main
+
+import (
+	"log"
+	"os"
+	"reflect"
+
+	"github.com/sanity-io/litter"
+	"github.com/urfave/cli"
+	calmwu_utils "github.com/wubo0067/calmwu-go/utils"
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+var (
+	logger *log.Logger
+)
+
+func init() {
+	logger = calmwu_utils.NewSimpleLog(nil)
+}
+
+func watchDeployment(clientSet *kubernetes.Clientset) {
+	dpWatcher, err := clientSet.AppsV1().Deployments("default").Watch(metav1.ListOptions{
+		LabelSelector: "app=test-scale-status",
+	})
+
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	for e := range dpWatcher.ResultChan() {
+		logger.Printf("event type[%s] event object[%s:%T]", e.Type, reflect.TypeOf(e).String(), e.Object)
+
+		if dpObj, ok := e.Object.(*appsv1.Deployment); ok {
+			logger.Printf("Deployment ObjectMeta[%s] status:%s", litter.Sdump(dpObj.ObjectMeta),
+				litter.Sdump(dpObj.Status))
+		}
+	}
+
+	return
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "watchdp"
+	app.Usage = "Watch Deployment"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "kubeconfig",
+			Value: "/root/.kube/config",
+			Usage: "k8s config",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		// 初始化log
+		kubeconfig := c.String("kubeconfig")
+		logger.Printf("kubeconfig:[%s]", kubeconfig)
+
+		config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			logger.Fatal(err.Error)
+		}
+
+		logger.Printf("config:%s", litter.Sdump(config))
+		clientSet, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			logger.Fatal(err.Error())
+		}
+
+		watchDeployment(clientSet)
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	logger.Printf("Watch Deployment exit!")
+}
